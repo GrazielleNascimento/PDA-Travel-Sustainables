9ff8fe8d2aab2405ef1353d6e802c9e0f7ce1f43
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-07-20 18:40:12 -0300

    first commit

---
+++ src/models/accomodation.model.js
@@ -0,0 +1,8 @@
+export class Acommodation{
+    constructor(id, name, type, location){
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.location = location;
+    }
+}
