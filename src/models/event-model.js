00fe35069e8010249ff53dfb37fd20b8d8cec784
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-07-20 19:07:59 -0300

    Add .gitignore entry for settings.json and create event model

---
+++ src/models/event-model.js
@@ -0,0 +1,9 @@
+export class Event{
+    constructor(id, name, type, description, location){
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.description = description;
+        this.location = location;
+    }
+}
