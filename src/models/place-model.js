fbefa29178d0fd5ed8c427c45b32e92ba925985f
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-07-20 19:08:32 -0300

    feat: add place model

---
+++ src/models/place-model.js
@@ -0,0 +1,9 @@
+export class Place{
+    constructor(id, name, type, description, location){
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.description = description;
+    this.location = location;
+    }
+}
