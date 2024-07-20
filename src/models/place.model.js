4cb79d25c23cbdca77357dea04bcc8041f192f1d
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-07-20 19:06:22 -0300

    feat: add accomodation model

--- src/models/place.model.js
+++
@@ -1,9 +0,0 @@
-export class Place{
-    constructor(id, name, type, description, location){
-    this.id = id;
-    this.name = name;
-    this.type = type;
-    this.description = description;
-    this.location = location;
-    }
-}
