ec7b94c425a81117f128e1e20fa276863e477def
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-07-20 19:08:41 -0300

    feat: add transportation model

---
+++ src/models/transportation-model.js
@@ -0,0 +1,8 @@
+export class Transportation{
+    constructor(id, name, type, location){
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.location = location;
+    }
+}
