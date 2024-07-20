ac7d81304b77b12d9107e8e3e432c9c541085361
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-07-20 19:14:18 -0300

    feat: add package model

---
+++ src/models/package-model.js
@@ -0,0 +1,8 @@
+export class Package{
+    constructor(id, name, type, description, location){
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.description = description
+    }
+}
