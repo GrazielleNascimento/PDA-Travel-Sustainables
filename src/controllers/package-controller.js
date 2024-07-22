5ff61e196f4c6fc808d52a1dc6504c45b1411519
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-07-21 21:42:08 -0300

    feat: Add package controller and routes for creating, retrieving, updating, and deleting packages

---
+++ src/controllers/package-controller.js
@@ -0,0 +1,93 @@
+import { Package } from '../models/package-model';
+import { readFileSync, writeFileSync } from 'fs';
+
+const packageFilePath = 'src/data/packages-data.json';
+
+export const createPackage = (id, name, type, description, location) => {
+    
+    const packageTravel = new Package(id, name, type, description, location);
+
+    const packageData = readDataFromFile();
+
+    packageData.packages.push(packageTravel);
+
+    saveDataToFile(packageData);
+    return packageTravel;
+}
+
+export const getPackages = () => {
+    const packageData = readDataFromFile();
+    console.log('fetching packages successfully');
+    return packageData.packages;
+}
+
+export const getPackageById = (id) => {
+    const packageData = readDataFromFile();
+    console.log('fetching packages successfully');
+    const packageTravel = packageData.packages.find((packageTravel) => packageTravel.id === id);
+
+    if(packageTravel){
+        console.log('fetching package successfully');
+
+        return packageTravel;
+    } else {
+        console.log('package not found');
+
+        return null;
+    }
+}
+ 
+export const updatePackage = (id, name, type, description, location) => {
+        
+        const packageData = readDataFromFile();
+    
+        const packageIndex = packageData.packages.findIndex((packageTravel) => packageTravel.id === id);
+    
+        if(packageIndex !== -1){
+            packageData.packages[packageIndex] = new Package(id, name, type, description, location);
+            saveDataToFile(packageData);
+            console.log('updating package successfully');
+            return packageData.packages[packageIndex];
+        } else {
+            console.log('package not found');
+            return null;
+        }
+    }
+
+    
+export const deletePackage = (id) => {
+
+    const packageData = readDataFromFile();
+
+    const packageIndex = packageData.packages.findIndex((packageTravel) => packageTravel.id === id);
+
+    if(packageIndex !== -1){
+        packageData.packages.splice(packageIndex, 1);
+        saveDataToFile(packageData);
+        console.log('deleting package successfully');
+        return true;
+    } else {
+        console.log('package not found');
+        return false;
+    }
+}
+
+const readDataFromFile = () => {
+    try {
+      const rawData = readFileSync(packageFilePath);
+      return JSON.parse(rawData);
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        console.log('Package file not found');
+        return { accomodations: [] };
+      } else {
+        throw err;
+      }
+    }
+  };
+  
+  
+  const saveDataToFile = (data) => {
+    writeFileSync((packageFilePath), JSON.stringify(data, null, 2));
+    console.log('Package saved to file');
+  };
---
+++ src/routes/package-router.js
@@ -0,0 +1,54 @@
+import { Router } from "express";
+import{ createPackage, getPackages, getPackageById, updatePackage, deletePackage } from "../controllers/package-controller.js";
+
+const packageRouter = Router();
+
+packageRouter.post('/create', (req, res) =>{
+    const { id, name, type, description, location} = req.body;
+    const packageTravel = createPackage(id, name, type, description, location);    
+
+    res.status(201).json(packageTravel);
+});
+    
+packageRouter.get('/getAll', ( res) => {
+    const packages = getPackages();
+
+    res.status(200).json(packages);
+});
+
+packageRouter.get('/getById/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const packageTravel = getPackageById(id);
+
+    if(packageTravel){
+        res.status(200).json(packageTravel);
+    } else {
+        res.status(404).json({ message: 'Package not found' });
+    }
+});
+
+packageRouter.put('/update/:id', (req, res) => {
+    
+    const id = parseInt(req.params.id);
+    const { name, type, description, location } = req.body;
+
+    const packageTravel = updatePackage(id, name, type, description, location);
+
+    if(packageTravel){
+        res.status(200).json(packageTravel);
+    } else {
+        res.status(404).json({ message: 'Package not found' });
+    }
+});;
+
+packageRouter.delete('/delete/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    if(deletePackage(id)){
+        res.status(204).json({})
+    } else {
+        res.status(404).json({ message: 'Package not found' });
+    }
+});
+
+export { packageRouter };
